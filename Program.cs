a2037c47cae85ca72710dc53fbdbd07de828d5c5
akmalhusain <70431817+akmalhusain@users.noreply.github.com>
2021-12-17 17:23:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+class listprogram
+{
+    static void Main(string[] args)
+    {
+        var name =new SortedSet<string>();
+        name.Add("akmal");
+        name.Add("shakib");
+        name.Add("sharukh");
+        name.Add("mailk");
+        name.Add("akmal");
+        Console.WriteLine("before remove\n");
+        foreach(var i in name)
+        {
+         Console.WriteLine(i);
+        }
+        name.Remove("mailk");
+        Console.WriteLine("after remove\n");
+        foreach (var i in name)
+
+            { 
+            
+            Console.WriteLine(i);
+        }
+    }
+}
